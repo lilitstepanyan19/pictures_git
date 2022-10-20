7c10ad22caae867297c47722e152d8df89fa33c0
lilitstepanyan19 <lil-lid@mail.ru>
2022-10-20 23:41:55 +0400

    update pictures.py

--- pictures.py
+++ pictures.py
@@ -1,19 +1,48 @@
 import pytesseract
 from PIL import Image
 
-img = Image.open('img.jpg')
-
 pytesseract.pytesseract.tesseract_cmd = r"C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe"
-text = pytesseract.image_to_string(img, lang='en')
+
+def image_open(img_name):
+    try:
+        if img_name[-4:] == '.jpg' or img_name[-4:] == '.png':
+            return Image.open(img_name)
+        else:
+            print('Image not found')
+            return 
+    except FileNotFoundError(err):
+        return err
 
 
-print(text)
+def image_to_str(img):
+    return pytesseract.image_to_string(img, lang='rus+eng')
 
-with open('file.txt', 'a') as f:
-    f.write(text)
+def write_file(fname, text):
+    if fname[-4:] != '.txt':
+        print('Write a correct filename')
+        return 
+    with open(fname, 'w') as f:
+        f.write(str(text.strip()))
+        return f
+    return
 
+def main():
+    img_name = 'img1.jpg'
+    fname = 'file.txt'
+        
+    img = image_open(img_name)
+    text = image_to_str(img)
 
+    write_file(fname, text)
+    return text.strip()
+# print(main())    
 
+if __name__ == '__main__':
+    main()    
+    
+    
+    
+    
 # import EasyOCR
 
 
